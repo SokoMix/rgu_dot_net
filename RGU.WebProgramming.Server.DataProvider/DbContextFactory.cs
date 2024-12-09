815a3c92297c084f39725c46afdda0548d291da4
SokoMix <dimonsheu@mail.ru>
2024-12-09 11:15:08 +0300

    AAAA

---
+++ RGU.WebProgramming.Server.DataProvider/DbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using RGU.WebProgramming.Server.PostgresStorage;
+using RGU.WebProgramming.Core.Interfaces;
+
+namespace RGU.WebProgramming.Server.DataProvider;
+
+public class DbContextFactory:
+    IAbstractFactory<PostgresStorage.PostgresStorage>
+{
+    
+    // TODO: constructor from IOptions<DbContextFactorySettings>
+
+    public PostgresStorage.PostgresStorage Create()
+    {
+        return new PostgresStorage.PostgresStorage();
+    }
+    
+}
